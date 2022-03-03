54fbb55a246a60a5ac9b5c75805320dbae02032c
ksy <tpdud3256@gmail.com>
2022-03-03 09:09:04 +0900

    치잉을해야하는이유 수정

---
+++ src/components/Reason/ReasonItem.js
@@ -0,0 +1,56 @@
+import { Stack, Typography } from "@mui/material";
+
+function ReasonItem({ title, text, imgUrl, id }) {
+    return (
+        <Stack spacing={"1vw"} justifyContent="center" alignItems="center">
+            <Typography
+                component="div"
+                sx={{
+                    color: "#fff379",
+                    fontFamily: "ROKAFSansBold",
+                    fontSize: "7vw",
+                }}
+            >
+                0{id + 1}
+            </Typography>
+
+            <Typography
+                component="div"
+                sx={{
+                    color: "#ffffff",
+                    fontFamily: "Cafe24Ohsquareair",
+                    fontSize: "5vw",
+                }}
+            >
+                {title}
+            </Typography>
+
+            <img
+                loading="lazy"
+                src={imgUrl}
+                style={{ width: "100%", paddingTop: 20 }}
+            ></img>
+
+            <Typography
+                component="div"
+                sx={{
+                    color: "#ffffff",
+                    fontFamily: "S-CoreDream-3Light",
+                    fontSize: "3vw",
+                    padding: 3,
+                }}
+            >
+                {text.split("\n").map((line, i) => {
+                    return (
+                        <span key={i}>
+                            {line}
+                            <br />
+                        </span>
+                    );
+                })}
+            </Typography>
+        </Stack>
+    );
+}
+
+export default ReasonItem;
---
+++ src/components/Reason/reasonInfo.js
@@ -0,0 +1,27 @@
+export const reasonInfo = [
+    {
+        title: "같이의 가치",
+        text: "영어가 안되는 가장 큰 이유는 바로 ‘중도포기’입니다.\n마라톤에는 페이스메이커, 헬스장에는 퍼스널트레이너가\n존재 하듯이 여러분들에게는 치어들이\n당신의 영어를 완주할 수 있도록 끝까지 함께합니다.",
+        imgUrl: "/assets/images/main/reason/outing.jpg",
+    },
+    {
+        title: "Outing Class",
+        text: "반복되는 일상!\n정기모임으로 영어라는 매개체를 통해\n다양한 사람들과의 만남이 주는 설렘을 느껴보세요.\n파티 &쿠킹 클래스, 원데이 클래스, 하이킹, 피크닉, 시즌 테마 활동들이 준비되어 있습니다.",
+        imgUrl: "/assets/images/main/reason/toghether.jpg",
+    },
+    {
+        title: "검증된 외국인 선생님과의 수업!",
+        text: "ESL 자격을 갖춘 외국인 선생님과의 수업을 통해\n단순히 제자리 걸음의 대화가 아닌\n실력을 높일 수 있는 과정이 준비되어 있습니다.",
+        imgUrl: "/assets/images/main/reason/online.jpg",
+    },
+    {
+        title: "내 일상과 함께하는 채팅클래스",
+        text: "주말에만 공부하는 일회성 영어는 그만~!\n평일에는 채팅클래스를 통해서 내 생각을 공유해봄으로써\nWriting 실력도 향상시킬 수 있습니다.",
+        imgUrl: "/assets/images/main/reason/chatting.PNG",
+    },
+    {
+        title: "4대 영역을 아우르는 커리큘럼",
+        text: "언어의 4대 영역을 골고루 갖춰야\n완벽한 영어 회화 공부라는 것을 알고 계셨나요?!\nListening, Speaking, Reading, Writing\n이 4가지를 한번에 잡아보세요~!\n어학연수의 효과를 가져가실 수 있습니다.",
+        imgUrl: "/assets/images/main/reason/curriculum.jpg",
+    },
+];
