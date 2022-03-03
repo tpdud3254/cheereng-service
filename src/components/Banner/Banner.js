82d59c7e398b8cfc3209bdc8283a99f6c2d722d1
ksy <tpdud3256@gmail.com>
2022-03-03 12:44:59 +0900

    배너 링크

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,23 +3,50 @@ import "react-awesome-slider/dist/styles.css";
 import "./style.css";
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import { Link } from "react-router-dom";
 
 function Banner() {
     const theme = useTheme();
     const matches = useMediaQuery(theme.breakpoints.up("md"));
+
+    function Div() {
+        return <div style={{ width: 100, height: 100 }}>dfhaklsdfhlaksdf</div>;
+    }
     return (
         <AwesomeSlider bullets={false} fillParent={false} className="aws-btn">
             <div
-                style={{ width: "100vw" }}
-                data-src={`/assets/images/main/banner/Banner1_2${
-                    matches ? "" : "_m"
-                }.png`}
+                style={{
+                    width: "100vw",
+                    margin: matches ? "1vw auto" : "",
+                    textAlign: "center",
+                }}
+                children={
+                    <Link to={`/3`} style={{ textDecoration: "none" }}>
+                        <img
+                            src={`/assets/images/main/banner/Banner1_2${
+                                matches ? "" : "_m"
+                            }.png`}
+                            style={{ width: "83%" }}
+                        ></img>
+                    </Link>
+                }
             />
             <div
-                style={{ width: "100vw" }}
-                data-src={`/assets/images/main/banner/Banner2_2${
-                    matches ? "" : "_m"
-                }.png`}
+                style={{
+                    width: "100vw",
+                    margin: matches ? "1vw auto" : "",
+                    textAlign: "center",
+                }}
+                children={
+                    <Link to={`/3`} style={{ textDecoration: "none" }}>
+                        <img
+                            src={`/assets/images/main/banner/Banner2_2${
+                                matches ? "" : "_m"
+                            }.png`}
+                            style={{ width: "83%" }}
+                        ></img>
+                    </Link>
+                }
             />
         </AwesomeSlider>
     );
