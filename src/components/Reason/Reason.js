773db90ad55f3501535a08b55ab28e5dec48db7e
ksy <tpdud3256@gmail.com>
2022-03-03 09:09:38 +0900

    치잉을해야하는이유 수정

--- src/components/Reason/Reason.js
+++ src/components/Reason/Reason.js
@@ -1,6 +1,8 @@
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import { reasonInfo } from "./reasonInfo";
+import ReasonItem from "./ReasonItem";
 function Reason() {
     const theme = useTheme();
     const matches = useMediaQuery(theme.breakpoints.up("md"));
@@ -12,20 +14,45 @@ function Reason() {
                 backgroundImage: `url(${"/assets/images/main/reason/reason_bg.png"})`,
                 backgroundSize: "contain",
                 width: "100vw",
+                paddingTop: matches ? "" : "10vw",
+                paddingBottom: matches ? "" : "10vw",
             }}
         >
-            <img
-                loading="lazy"
-                src={`/assets/images/main/reason/${
-                    matches ? "reason" : "reason_m"
-                }.png`}
-                style={{
-                    width: matches ? "80%" : "98%",
-                    margin: matches ? "6vw" : "",
-                    marginTop: matches ? "" : "6vw",
-                    marginBottom: matches ? "" : "6vw",
-                }}
-            ></img>
+            {matches ? (
+                <img
+                    loading="lazy"
+                    src="/assets/images/main/reason/reason.png"
+                    style={{
+                        width: "80%",
+                        margin: "6vw",
+                    }}
+                ></img>
+            ) : (
+                <Stack
+                    spacing={"4vw"}
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    <img
+                        loading="lazy"
+                        src="/assets/images/main/reason/title.png"
+                        style={{
+                            width: "70vw",
+                            margin: matches ? "6vw" : "",
+                        }}
+                    ></img>
+
+                    {reasonInfo.map((item, index) => (
+                        <ReasonItem
+                            key={index}
+                            id={index}
+                            title={item.title}
+                            text={item.text}
+                            imgUrl={item.imgUrl}
+                        />
+                    ))}
+                </Stack>
+            )}
         </Box>
     );
 }
